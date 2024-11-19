2d6f89d005a94e6b148784504dc71ce4a8d44b38
ibukunanih <ibukunanih@gmail.com>
2024-11-19 16:43:50 +0000

    First Commit

---
+++ members/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+def main(requests):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def members(requests):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("all_members.html")
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, requests))
+
+def details(requests, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, requests))
